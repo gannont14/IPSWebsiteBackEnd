3f83350e512873c43a2c5b4bdd3e7f8eabff2d77
gtubbs1414 <tubbsgannon14@gmail.com>
2024-07-17 21:54:13 -0400

    added put and patch request supporot for photo and services views

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import status
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from rest_framework.decorators import api_view, parser_classes
 
 
@@ -17,9 +17,9 @@ def getRoutes(request):
     return Response('TestApi')
 
 # currently just a get and post available
-@api_view(['GET', 'POST'])
-# used for uploading images
-@parser_classes([MultiPartParser, FormParser])
+@api_view(['GET', 'POST', 'PUT', 'PATCH'])
+# used for uploading images, as well as parsing JSON request to put and patch requests
+@parser_classes([MultiPartParser, FormParser, JSONParser])
 def getServices(request):
     if request.method == 'GET':
         services = Service.objects.all()
@@ -32,12 +32,23 @@ def getServices(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+    elif request.method in  ['PUT', 'PATCH']:
+        itemId = request.data.get('id')
+        try:
+            item = Service.objects.get(id=itemId)
+        except Service.DoesNotExist:
+             return Response({'error': 'Item not found'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = ServiceSerializer(item, data=request.data, partial=(request.method == 'PATCH'))
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 # View to collect all of the photos for the photo gallery
-@api_view(['GET', 'POST'])
-@parser_classes([MultiPartParser, FormParser])
+@api_view(['GET', 'POST', 'PUT', 'PATCH'])
+@parser_classes([MultiPartParser, FormParser, JSONParser])
 def getPhotos(request):
     if request.method == 'GET':
         photos = Photo.objects.all()
@@ -48,4 +59,18 @@ def getPhotos(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method in ['PUT', 'PATCH']:
+        itemId = request.data.get('id')
+        if not itemId:
+            return Response({'error': 'Item id not present'}, status=status.HTTP_404_NOT_FOUND)
+        try:
+            item = Photo.objects.get(id=itemId)
+        except Photo.DoesNotExist:
+             return Response({'error': 'Item not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = PhotoSerializer(item, data=request.data, partial=(request.method == 'PATCH'))
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
