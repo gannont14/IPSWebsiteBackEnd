21e10c26cf4f33626c2c0bb440463ec399d832db
gannont <gannon.tubbs@e-gineering.com>
2024-08-02 15:18:42 -0400

    added admin authentication

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
+
+
 from . import views
 
 from .models import Service
@@ -9,7 +13,9 @@ urlpatterns = [
     path('', views.getRoutes, name="routes"),
     path('api/services', views.getServices, name="services"),
     path('api/photos', views.getPhotos, name="photos"),
-    path('api/aboutMainContent', views.getAboutMainContent, name="aboutMainContent")
+    path('api/aboutMainContent', views.getAboutMainContent, name="aboutMainContent"),
+    path('api/check_authentication', views.check_authentication, name="check_authentication"),
+    path('api/login',  views.login_view, name='login_view')
 ]
 
 if settings.DEBUG:
