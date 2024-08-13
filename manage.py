ae6174649ba1ed8ba97b73ddcdbc62b77cfa61de
gannont <gannon.tubbs@e-gineering.com>
2024-08-13 16:00:48 -0400

    updated port settings

--- manage.py
+++ manage.py
@@ -7,6 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IPSWebBackend.settings')
+    port = os.getenv('DJANGO_PORT', '8000')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,7 +16,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+    execute_from_command_line(['manage.py', 'runserver', f'0.0.0.0:{port}'])
 
 
 if __name__ == '__main__':
