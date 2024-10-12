5dcca84ff7d3ae862f1cab99d3cd243806034e28
gannont <gannon.tubbs@e-gineering.com>
2024-10-12 13:21:01 -0400

    updated settings.py for media files

--- IPSWebBackend/urls.py
+++ IPSWebBackend/urls.py
@@ -24,3 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
